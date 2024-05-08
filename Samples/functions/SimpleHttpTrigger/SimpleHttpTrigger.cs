54b142ef4d6c81fa8d26a390e8b345f5749f14f8
Stijn Peeters <schwarzie2478@hotmail.com>
2024-05-08 23:50:58 +0200

    return qr code from sample

--- Samples/functions/SimpleHttpTrigger/SimpleHttpTrigger.cs
+++ Samples/functions/SimpleHttpTrigger/SimpleHttpTrigger.cs
@@ -2,6 +2,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
 
 namespace AZ204.Samples.Functions
 {
@@ -17,8 +22,20 @@ namespace AZ204.Samples.Functions
         [Function("SimpleHttpTrigger")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("C# HTTP trigger function processed a requestfor a new QRCode.");
+            string? text = req.Query.ContainsKey("text") ? req.Query["text"] : string.Empty; // Use null-conditional operator to safely access the value and provide a default value if it is null
+            //log text
+            _logger.LogInformation($"Text: {text}");
+
+            byte[] qrCode = text != null ? GenerateQRCode(text) : new byte[0]; // Add null check for 'text' parameter
+            return new FileContentResult(qrCode, "image/png");
         }
+        public static byte[] GenerateQRCode(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.PngByteQRCode(qrCodeData);
+            return qrCode.GetGraphic(20);// Adjust the size as needed
+        }        
     }
 }
---
+++ docs/Code/Install Package Dependency loop detected for package XXX.md
@@ -0,0 +1,21 @@
+---
+status: seedling
+dg-publish: true
+tags:
+  - code/dotNET
+creation_date: 2024-05-08 22:48
+definition: undefined
+ms-learn-url: undefined
+url: undefined
+aliases:
+---
+
+| MetaData   |                                              |
+| ---------- | -------------------------------------------- |
+| Definition | `VIEW[{definition}][text(renderMarkdown)]`   |
+| Homesite   | `VIEW[{url}][text(renderMarkdown)]`          |
+| MS Learn   | `VIEW[{ms-learn-url}][text(renderMarkdown)]` |
+
+Problem occured inside powershell terminal of VS Code  [[install-package]] failed for package [[QRCoder]]
+
+doing the install with [[dotnet cli ]]did work
